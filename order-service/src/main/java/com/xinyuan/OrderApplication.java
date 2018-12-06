ca2617b084405b8f255f2a7a244d42ceed681ed1
User <123@qq.com>
2018-12-06 15:25:36 +0800

    hwz

--- order-service/src/main/java/com/xinyuan/OrderApplication.java
+++ order-service/src/main/java/com/xinyuan/OrderApplication.java
@@ -1,9 +1,12 @@
 package com.xinyuan;
 
+import org.apache.catalina.filters.RequestDumperFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 /**
  * 订单服务
@@ -19,4 +22,13 @@ public class OrderApplication {
         SpringApplication.run(OrderApplication.class, args);
     }
 
+    /**
+     * 为测试环境添加相关的 Request Dumper information，便于调试
+     * @return
+     */
+    @Profile("!cloud")
+    @Bean
+    RequestDumperFilter requestDumperFilter() {
+        return new RequestDumperFilter();
+    }
 }
