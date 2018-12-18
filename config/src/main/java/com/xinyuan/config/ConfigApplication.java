b5c0ec753d1c7509de8fb586de9fa5ffe8b994ea
马睿隆 <774328384@qq.com>
2018-12-18 19:03:15 +0800

    增加Elsearch

--- config/src/main/java/com/xinyuan/config/ConfigApplication.java
+++ config/src/main/java/com/xinyuan/config/ConfigApplication.java
@@ -19,13 +19,14 @@ package com.xinyuan.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @Author: hwz
  * @Date: Created in 11:40 2018/12/18.
  */
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
