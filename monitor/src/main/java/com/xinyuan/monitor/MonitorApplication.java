f9c6df5b86901f21fb9a64471c016c9875ff6cfe
马睿隆 <774328384@qq.com>
2018-12-20 16:18:09 +0800

    jiankong 重构完成

--- monitor/src/main/java/com/xinyuan/monitor/MonitorApplication.java
+++ monitor/src/main/java/com/xinyuan/monitor/MonitorApplication.java
@@ -3,13 +3,14 @@ package com.xinyuan.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
 /**
  * @Author: hwz
  * @Date: Created in 10:03 2018/12/17.
  */
 @EnableAdminServer
-@SpringBootApplication
+@SpringCloudApplication
 public class MonitorApplication {
     public static void main(String[] args) {
         SpringApplication.run(MonitorApplication.class, args);
