7302c87c656f8577ad73b724d65adbf207fd2318
马睿隆 <774328384@qq.com>
2018-12-20 16:01:36 +0800

    registry重构完成

--- registry/src/main/java/com/xinyuan/RegistryApplication.java
+++ registry/src/main/java/com/xinyuan/RegistryApplication.java
@@ -2,6 +2,7 @@ package com.xinyuan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -10,7 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @author liang
  */
 @EnableEurekaServer
-@SpringBootApplication
+@SpringCloudApplication
 public class RegistryApplication {
     public static void main(String[] args) {
         SpringApplication.run(RegistryApplication.class, args);
