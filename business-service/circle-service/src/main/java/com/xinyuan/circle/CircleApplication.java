f868da4ebbe54dc513f4696392d7c74c9340a38f
马睿隆 <774328384@qq.com>
2018-12-20 18:03:38 +0800

    事务管理配置修改

--- business-service/circle-service/src/main/java/com/xinyuan/circle/CircleApplication.java
+++ business-service/circle-service/src/main/java/com/xinyuan/circle/CircleApplication.java
@@ -1,11 +1,8 @@
 package com.xinyuan.circle;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
