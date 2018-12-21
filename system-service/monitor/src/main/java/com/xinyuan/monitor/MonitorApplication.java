e9e9074259bdf2b2eb0a3f098b2e2a869a6e4000
马睿隆 <774328384@qq.com>
2018-12-21 16:06:44 +0800

    config配置中心自动刷新

--- system-service/monitor/src/main/java/com/xinyuan/monitor/MonitorApplication.java
+++ system-service/monitor/src/main/java/com/xinyuan/monitor/MonitorApplication.java
@@ -3,14 +3,14 @@ package com.xinyuan.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @Author: hwz
  * @Date: Created in 10:03 2018/12/17.
  */
 @EnableAdminServer
-@SpringCloudApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MonitorApplication {
     public static void main(String[] args) {
         SpringApplication.run(MonitorApplication.class, args);
